bd5abd0ebbc1945578e1fd470fc47fbd2e26e5c2
John Chou <john0724@gmail.com>
2021-03-10 10:38:54 +0800

    整個畫面只有一個Scroll，CollectionView的內容再多也不會再長出另一個Scroll去影響User的操作

--- TabBarSample/TabBarSample/ShareViewControllers/EZShareViewController.swift
+++ TabBarSample/TabBarSample/ShareViewControllers/EZShareViewController.swift
@@ -10,6 +10,8 @@ enum EZSomeType: Int {case search, create}
 class EZShareViewController: UIViewController {
     @IBOutlet weak var searchContainerView: UIView!
     @IBOutlet weak var createContainerView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewLayoutConstriant: NSLayoutConstraint!
     var data: String?
     var displayMode: EZSomeType = .search
     var buttonClickActionHandler: (() -> Void)?
@@ -26,6 +28,13 @@ class EZShareViewController: UIViewController {
         self.title = data
         setupUI()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Collection View 的內容變動時會被叫用，取得內容大小，調整我們為它設定的LayoutConstriant.constant
+        let height = collectionView.collectionViewLayout.collectionViewContentSize.height
+        collectionViewLayoutConstriant.constant = height
+    }
     func setupUI() {
         searchContainerView.isHidden = !(displayMode == .search)
         createContainerView.isHidden = !(displayMode == .create)
@@ -41,4 +50,17 @@ class EZShareViewController: UIViewController {
             buttonClickActionHandler!()
         }
     }
+}
+
+extension EZShareViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Int.random(in: 0...20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellIdentifier", for: indexPath)
+        return cell
+    }
+    
+    
 }
